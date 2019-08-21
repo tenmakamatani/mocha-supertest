f0d3b3ce1ce8bdad3325cbb11eee463955888dab
tenmakamatani <tenmakamatani0@gmail.com>
2019-08-21 15:06:02 +0900

    [add]test case

--- index.js
+++ index.js
@@ -7,4 +7,10 @@ app.get("/", (req, res) => {
     res.status(200).send("ok");
 });
 
+app.post("/", (req, res) => {
+    const name = req.body.name;
+    const returnName = "Mr." + name;
+    res.status(200).send({ name: returnName });
+});
+
 module.exports = app;
--- test/test.js
+++ test/test.js
@@ -15,3 +15,19 @@ describe("GET /", () => {
     });
 
 });
+
+describe("POST /", () => {
+
+    it("Whether the string 'Mr.hoge' will return", (done) => {
+        request(app)
+            .post("/")
+            .send({ name: "hoge" })
+            .set("Accept", "application/json")
+            .expect("Content-Type", /json/)
+            .expect(200)
+            .then((res) => {
+                assert.equal(res.body.name, "Mr.hoge");
+            });
+    });
+
+});
